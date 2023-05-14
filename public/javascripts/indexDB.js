df584105184b96ea8056b6d4d184ef151892fc24
XRX <rxu37@sheffield.ac.uk>
2023-05-14 21:12:38 +0100

    Added operations for index db

--- public/javascripts/indexDB.js
+++ public/javascripts/indexDB.js
@@ -12,32 +12,118 @@ function openDB(dbName, version = 1) {
 
         request.onsuccess = function (event) {
             db = event.target.result;
-            console.log("open indexDB success");
+            console.log("Index DB opened successfully.");
             resolve(db);
         };
 
         request.onerror = function (event) {
-            console.log("failed to open indexDB");
+            console.log("Failed to open indexDB.");
         };
 
         request.onupgradeneeded = function (event) {
 
-            console.log("onupgradeneeded");
+            console.log("DB updating");
             db = event.target.result;
-            var objectStore;
+            var objectStoreSighting;
+            var objectStoreChat;
 
-            objectStore = db.createObjectStore("sighting", {
-                keyPath: "sightingId",
+            objectStoreSighting = db.createObjectStore("sighting", {
+                keyPath: "id",
                 autoIncrement: true
             });
 
-            objectStore.createIndex("date", "date", {unique: false});
-            objectStore.createIndex("identification", "identification", {unique: false});
-            objectStore.createIndex("location", "location", {unique: false});
-            objectStore.createIndex("user", "user", {unique: false});
-            objectStore.createIndex("description", "description", {unique: false});
-            objectStore.createIndex("img", "img", {unique: false});
-            objectStore.createIndex("password", "password", {unique: false});
+            objectStoreSighting.createIndex("date", "date", {unique: false});
+            objectStoreSighting.createIndex("identification", "identification", {unique: false});
+            objectStoreSighting.createIndex("location", "location", {unique: false});
+            objectStoreSighting.createIndex("user", "user", {unique: false});
+            objectStoreSighting.createIndex("description", "description", {unique: false});
+            objectStoreSighting.createIndex("img", "img", {unique: false});
+            objectStoreSighting.createIndex("password", "password", {unique: false});
+
+            objectStoreChat = db.createObjectStore("chat", {
+                keyPath: "id",
+                autoIncrement: true
+            });
+
+            objectStoreChat.createIndex("room_num", "room_num", {unique: false});
+            objectStoreChat.createIndex("user", "user", {unique: false});
+            objectStoreChat.createIndex("text", "text", {unique: false});
         };
     });
+}
+
+function  addData(db, storeName, data) {
+    var request = db
+        .transaction([storeName],"readwrite")
+        .objectStore(storeName)
+        .add(data)
+    request.onsuccess = function (event) {
+        console.log("Data has been added to "+storeName+".")
+    }
+    request.onerror = function (event) {
+        console.log("Failed to add data.")
+    }
+}
+
+function getDataById(db, storeName, id) {
+    return new Promise((resolve,reject) => {
+        var transaction = db.transaction([storeName])
+        var objectStore = transaction.objectStore(storeName)
+        var request = objectStore.get(id)
+
+        request.onsuccess = function (event) {
+            console.log("Success! "+request.result)
+            resolve(request.result)
+        }
+        request.onerror = function (event) {
+            console.log("Failed to get data.")
+        }
+    })
+}
+
+function getDataByIndex(db, storeName, indexName, indexValue) {
+    var store = db.transaction(storeName, "readwrite").objectStore(storeName);
+    var request = store.index(indexName).get(indexValue);
+    request.onerror = function () {
+        console.log("Failed to get data.");
+    };
+    request.onsuccess = function (e) {
+        var result = e.target.result;
+        console.log("Success! ", result);
+    };
+}
+
+function getAllData(db, storeName) {
+    let list = [];
+    var store = db
+        .transaction(storeName, "readwrite")
+        .objectStore(storeName);
+    var request = store.openCursor();
+    request.onsuccess = function (e) {
+        var cursor = e.target.result;
+        if (cursor) {
+            list.push(cursor.value);
+            cursor.continue();
+        } else {
+            console.log("Data: ", list);
+        }
+    };
+}
+
+function updateDB(db, storeName, data) {
+    var request = db
+        .transaction([storeName], "readwrite")
+        .objectStore(storeName)
+        .put(data);
+
+    request.onsuccess = function () {
+        console.log("Update succeed.");
+    };
+    request.onerror = function () {
+        console.log("Update failed");
+    };
+}
+
+function closeDB(db) {
+    db.close();
 }
