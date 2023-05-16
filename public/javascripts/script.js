7c17d4013309baeff4ac1708ddfc17f9ee69e4dc
Xingming Chen <Xchen208@sheffield.ac.uk>
2023-05-16 23:02:37 +0100

    Apply sorted function to date, user and id, still test the sorted for location. Still testing on manifest.json to let it display the correct icon. Testing the location function.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,7 +2,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const search = document.querySelector('.input-group input'),
         tableBody = document.querySelector('.table_body tbody'),
-        table_rows = document.querySelectorAll('tbody tr');
+        table_rows = document.querySelectorAll('tbody tr'),
+        table_headings = document.querySelectorAll('thead th');
 
     search.addEventListener('input', searchTable);
 
@@ -25,4 +26,32 @@ function searchTable(){
         visible_row.style.backgroundColor = (i % 2 ==0) ? 'transparent' : '#0000000b';
     });
 }
+
+    table_headings.forEach((head,i)=>{
+        let sort_arc = true;
+        head.onclick = ()=>{
+            table_headings.forEach(head => head.classList.remove('active'));
+            head.classList.add('active');
+
+            document.querySelectorAll('td').forEach(td => td.classList.remove('active'))
+            table_rows.forEach(row => {
+                row.querySelectorAll('td')[i].classList.add('active');
+            })
+
+            head.classList.toggle('asc', sort_arc)
+            sort_arc = head.classList.contains('asc') ? false : true;
+
+            sortTable(i,sort_arc);
+        }
+    })
+
+    function sortTable(column,sort_arc){
+        [...table_rows].sort((a,b) => {
+            let first_row = a.querySelectorAll('td')[column].textContent.toLowerCase(),
+                second_row = b.querySelectorAll('td')[column].textContent.toLowerCase();
+
+            return sort_arc ? (first_row < second_row ? -1 : 1) : (first_row < second_row ? -1 : 1);
+        })
+            .map(sorted_row => document.querySelector('tbody').appendChild(sorted_row));
+    }
 });
